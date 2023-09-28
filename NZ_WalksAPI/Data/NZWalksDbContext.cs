cb3de32d5866066200ac286cf127051daaa73f26
Aliecake <neyabenz@gmail.com>
2023-09-28 06:52:30 -0400

    DBContext

---
+++ NZ_WalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZ_WalksAPI.Models.Domain;
+
+namespace NZ_WalksAPI.Data
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
--- NZ_WalksAPI/Models/Domain/Walk.cs
+++ NZ_WalksAPI/Models/Domain/Walk.cs
@@ -6,5 +6,14 @@
         public required string Name { get; set; }
         public required string Description { get; set; }
         public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+
+        public Guid RegionId { get; set; }
+
+        //Navigation Property
+
+        public required Difficulty Difficulty { get; set; }
+        public required Region Region { get; set; }
+
     }
 }
